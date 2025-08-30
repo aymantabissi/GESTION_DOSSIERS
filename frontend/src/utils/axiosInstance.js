9b437ae9880076697ac075687b3fdf8eae890705
aymantabissi <aymanraisse7@gmail.com>
2025-08-30 02:13:54 +0200

    api

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 const axiosInstance = axios.create({
-  baseURL: `${API_BASE_URL}`, // /api included here
+  baseURL: API_BASE_URL, // /api included here
   withCredentials: true
 });
 
